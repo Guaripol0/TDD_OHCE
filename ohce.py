30a37877a5925b3de1a1f9eb615aadb16dde3960
Esteban Chandia <136018308+Guaripol0@users.noreply.github.com>
2025-04-07 17:43:36 -0400

    minimal implementation to pass the stop test

--- ohce.py
+++ ohce.py
@@ -1,4 +1,6 @@
 def echo(text, name=""):
+    if text == "Stop!":
+        return f"Adios {name}"
     reversed_text = text[::-1]
     if text == reversed_text:
         return f"{reversed_text}\n¡Bonita palabra!"
