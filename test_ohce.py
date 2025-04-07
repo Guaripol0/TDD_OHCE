f52eebcb0b00a629b1703971dd32faa20e0555f8
Esteban Chandia <136018308+Guaripol0@users.noreply.github.com>
2025-04-07 18:08:20 -0400

    modificacion de los test sin mocks

--- test_ohce.py
+++ test_ohce.py
@@ -1,11 +1,10 @@
-from unittest.mock import patch
 from datetime import datetime
-import pytest
 import ohce
+
 def test_echo_reversed_word():
-    assert ohce.echo("hello") == "hello"[::-1]
-    assert ohce.echo("world") == "world"[::-1]
-    assert ohce.echo("python") == "python"[::-1]
+    assert ohce.echo("hello") == "olleh"
+    assert ohce.echo("world") == "dlrow"
+    assert ohce.echo("python") == "nohtyp"
 
 def test_echo_palindrome_word():
     assert ohce.echo("madam") == "madam\n¡Bonita palabra!"
@@ -15,26 +14,20 @@ def test_echo_palindrome_word():
 def test_stop_returns_goodbye():
     assert ohce.echo("Stop!", name="Pedro") == "Adios Pedro"
 
-def mock_datetime(hour):
+def hora_mock(hour):
     return datetime(2025, 4, 7, hour, 0, 0)
 
-@patch("ohce.datetime")
-def test_saludo_dias(mock_dt):
-    mock_dt.now.return_value = mock_datetime(10)
-    mock_dt.now.return_value.time = lambda: mock_datetime(10).time()
-    salida = saludo("Pedro", "10:00")
-    assert salida.startswith("¡Buenos días Pedro!")
+def test_saludo_dias():
+    hora = hora_mock(10)
+    salida = ohce.run_ohce("Pedro", hora)
+    assert salida == "¡Buenos días Pedro!"
 
-@patch("ohce.datetime")
-def test_saludo_tardes(mock_dt):
-    mock_dt.now.return_value = mock_datetime(15)
-    mock_dt.now.return_value.time = lambda: mock_datetime(15).time()
-    salida = saludo("Pedro", "15:00")
-    assert salida.startswith("¡Buenas tardes Pedro!")
+def test_saludo_tardes():
+    hora = hora_mock(15)
+    salida = ohce.run_ohce("Pedro", hora)
+    assert salida == "¡Buenas tardes Pedro!"
 
-@patch("ohce.datetime")
-def test_saludo_noches(mock_dt):
-    mock_dt.now.return_value = mock_datetime(21)
-    mock_dt.now.return_value.time = lambda: mock_datetime(21).time()
-    salida = saludo("Pedro", "21:00")
-    assert salida.startswith("¡Buenas noches Pedro!")
+def test_saludo_noches():
+    hora = hora_mock(21)
+    salida = ohce.run_ohce("Pedro", hora)
+    assert salida == "¡Buenas noches Pedro!"
